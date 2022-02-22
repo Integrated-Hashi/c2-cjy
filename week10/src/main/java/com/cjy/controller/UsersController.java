8df2e4727e510eaab1d66d17cf98173a4c7f2fde
chen <chenjunyu_cqupt@outlook.com>
2022-02-22 14:08:58 +0800

    SpringMVC配置成功, tomcat运行成功

--- week10/src/main/java/com/cjy/controller/UsersController.java
+++ week10/src/main/java/com/cjy/controller/UsersController.java
@@ -1,4 +1,14 @@
 package com.cjy.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users")
 public class UsersController {
+    @RequestMapping("/findUsers")
+    public String findUsers(){
+        System.out.println("表现层：查询用户");
+        return "Users";
+    }
 }
---
+++ week10/src/main/webapp/WEB-INF/pages/Users.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: chen
+  Date: 2022/2/22
+  Time: 2:06 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" isELIgnored="false" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h3>查询所有用户</h3>
+</body>
+</html>
+
+
